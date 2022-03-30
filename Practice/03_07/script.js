18ea200ec9456f2e0e24bffe2c2b05ee32426de7
MM-Morsi <69103168+MM-Morsi@users.noreply.github.com>
2022-03-30 11:28:44 +0200

    first practice file
    adding some objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,25 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// people 
+const person = {
+    name: "ahmed",
+    age: "25"
+}
+
+// office
+const disk = {
+    color: "brown",
+    type: "cubicle",
+    dimensions: {
+        width: 0.5,
+        height: 1
+    }
+}
+
+// devices 
+const device = {
+    name: "laptop",
+    price: 4000
+}
